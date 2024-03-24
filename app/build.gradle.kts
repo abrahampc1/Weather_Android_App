3b10a8b68feae1a5e528a13d4f3963ba813fc136
abrahampc1 <abrahamjesus069@gmail.com>
2024-03-23 23:18:20 -0700

    Clase 371: Diseño inicial parte I

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.weather_android_app"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.weather_android_app"
